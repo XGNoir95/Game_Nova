66e743395e8b50aad3b125f0a775a877ada7d640
XGNoir95 <shuhridabrar2016@gmail.com>
2024-06-19 00:22:23 +0600

    backend almost done

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,14 @@ const User = require("./routes/user");
 const Games = require("./routes/game");
 const Favourites = require("./routes/favourite");
 const Cart = require("./routes/cart");
+const Order = require("./routes/order");
 app.use(express.json());
 //route handling
 app.use("/api/v1", User);
 app.use("/api/v1", Games);
 app.use("/api/v1", Favourites);
 app.use("/api/v1", Cart);
+app.use("/api/v1", Order);
 
 //creating Port
 app.listen(process.env.PORT,()=>{
