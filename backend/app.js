557b810ad2a1e4b12e8e397eb2913753894b7260
XGNoir95 <shuhridabrar2016@gmail.com>
2024-06-18 20:50:22 +0600

    Adding favourite apis

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,12 @@ require("./conn/conn");
 
 const User = require("./routes/user");
 const Games = require("./routes/game");
+const Favourites = require("./routes/favourite");
 app.use(express.json());
 //route handling
 app.use("/api/v1", User);
 app.use("/api/v1", Games);
+app.use("/api/v1", Favourites);
 
 //creating Port
 app.listen(process.env.PORT,()=>{
