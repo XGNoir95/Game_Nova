bb02d7cf9e699ad6ee103ca51653680bdb59131d
mahadir <mahadirmrh04@gmail.com>
2024-07-10 05:05:01 +0600

    Profile Fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -8,8 +8,22 @@ import AllGame from "./pages/AllGame";
 import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUP";
+import { useDispatch,useSelector } from "react-redux";
+import { authActions } from "./Store/auth";
 
 const App = () => {
+  const dispatch =useDispatch();
+  const role = useSelector((state)=> state.auth.role);
+  useEffect(()=>{
+    if(
+      localStorage.getItem("id")&&
+      localStorage.getItem("token")&&
+      localStorage.getItem("role")
+    ){
+      dispatch(authActions.login());
+      dispatch(authActions.changeRole(localStorage.getItem("role")));
+    }
+  },[]);
   return (
     <div>
       
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Sidebar from '../components/Profile/Sidebar';
+import { Outlet } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector } from 'react-redux'
+import Loader from '../components/Loader/loader';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [Profile,setProfile]=useState();
+  const headers = {
+    id: localStorage.getItem('id'),
+    authorization: `Bearer ${localStorage.getItem('token')}`,
+  };
 
-export default Profile
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:1000/api/v1/get-user-information',
+          { headers }
+        );
+        console.log('Fetched Profile data:', response.data);
+        setProfile(response.data);
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      }
+    };
+    fetch();
+  }, []);
+  
+
+  return (
+    <div className="bg-zinc-900 px-2 md:px-12 flex flex-col md:flex-row h-screen py-8 gap-4 text-white">
+      {!Profile &&(
+        <div className='w-full h-[100%] flex items-center justify-center'>
+          <Loader />
+        </div>
+        )}
+      {Profile&&(
+        <>
+        <div className="w-full md:w-1/6">
+          <Sidebar data={Profile} />
+        </div>
+        <div className="w-full md:w-5/6">
+          <Outlet />
+        </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
