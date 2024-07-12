47b6a075c89a389b2f183b11f6dd5135a082a324
Shuhrid Abrar <139622785+XGNoir95@users.noreply.github.com>
2024-07-12 20:59:01 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const Cart = require("./routes/cart");
 const Order = require("./routes/order");
 
 app.use(cors({
-    origin: ["https://gn-old-front.vercel.app"],
+    origin: ["https://game-nova-front.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
