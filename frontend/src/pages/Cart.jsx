38f335aa867f7fbe098fc44eea633ed651c120bf
mahadir <mahadirmrh04@gmail.com>
2024-08-12 17:45:40 +0600

    Implementing cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,12 +1,115 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
+import Loader from '../components/Loader/loader';
+import { AiFillDelete } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  return (
-    <div 
-      className="min-h-screen bg-cover bg-center text-white px-10 py-8" 
-      style={{ backgroundImage: `url('bg2.jpg')` }}
-    >Cart</div>
-  )
-}
+  const navigate = useNavigate();
+  const [Cart, setCart] = useState();
+  const [Total, setTotal] = useState(0);
+  const headers = {
+    id: localStorage.getItem('id'),
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
 
-export default Cart
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await Axios.get("http://localhost:1000/api/v1/get-user-cart", { headers });
+        console.log(response.data.games);
+        setCart(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetch();
+  }, []);
+
+  const deleteItem = async (gameId) => {
+    try {
+      const response = await Axios.put(`http://localhost:1000/api/v1/remove-game-from-cart/${gameId}`, {}, { headers });
+      setCart(Cart.filter(item => item._id !== gameId));
+      alert(response.data.message);
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+  useEffect(()=> {
+    if (Cart && Cart.length > 0){
+      let total = 0;
+      Cart.map((items)=>{
+        total += items.price;
+      });
+      setTotal(total);
+      total = 0;
+    }
+  },[Cart]);
+
+  const PlaceOrder = async() =>{
+    try{
+      const response = await Axios.post(`http://localhost:1000/api/v1/place-order`, { order: Cart }, { headers });
+      alert(response.data.message);
+      navigate("/profile/orderHistory");
+    }catch(error){
+      console.log(error);
+    }
+  };
+  return (
+    <div className = "bg-zinc-900 px-12 py-8 h-screen">
+      {!Cart && <div className = "w-full h-[100%] flex items-center justify-center"><Loader /></div>}
+      {Cart && Cart.length === 0 && (
+        <div className="h-screen">
+          <div className="h-[100%] flex items-center justify-center flex-col">
+            <h1 className="text-5xl lg:text-6xl font-semibold text-zinc-400">Empty Cart</h1>
+          </div>
+        </div>
+      )}
+      {Cart && Cart.length > 0 && (
+        <>
+          <h1 className="text-5xl font-semibold text-zinc-500 mb-8">Your Cart</h1>
+          {Cart.map((items, i) => (
+            <div className="w-full my-4 rounded flex flex-col md:flex-row p-4 bg-zinc-800 justify-between items-center" key={i}>
+              <img src={items.url} alt="/" className="h-[20vh] md:h-[10vh] object-cover" />
+              <div className="w-full md:w-auto">
+                <h1 className="text-wxl text-zinc-100 font-semibold text-start mt-2 md:mt-0">{items.title}</h1>
+                <p className="text-normal text-zinc-400 mt-2 hidden lg:block">{items.desc.slice(0, 100)}...</p>
+                <p className="text-normal text-zinc-300 mt-2 hidden md:block lg:hidden">{items.desc.slice(0, 65)}...</p>
+                <p className="text-normal text-zinc-300 mt-2 md:hidden">{items.desc.slice(0, 100)}...</p>
+              </div>
+              <div className="flex mt-4 w-full md:w-auto items-center justify-between">
+                <h2 className="text-zinc-100 text-3xl font-semibold flex">${items.price} USD</h2>
+                <button className="bg-red-100 text-red-700 border border-red-700 rounded p-2 ms-12" onClick={() => deleteItem(items._id)}>
+                  <AiFillDelete />
+                </button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
+      {Cart && Cart.length > 0 && (
+        <div className = "mt-4 w-full flex items-center justify-end">
+          <div className  = "p-4 bg-zinc-800 rounded">
+            <h1 className = "text-3xl text-zinc-200 font semibold">
+              Total Amount
+            </h1>
+            <div className = "mt-3 flex items-center justify-between text-xl text-zinc-200">
+              <h2>{Cart.length} books</h2><h2>$ {Total}</h2>
+            </div>
+            <div className = "w-[100%] mt-3">
+              <button 
+                className = "bg-purple-900 flex justify-center w-full font-semibold py-2 px-4 rounded hover:bg-zinc-200"
+                onClick = {PlaceOrder}
+              >
+                Place Your Order
+              </button>
+            </div>
+
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
