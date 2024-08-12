9751ef1db4c72e9599802b1282edcae721892a3a
mahadir <mahadirmrh04@gmail.com>
2024-08-12 18:11:58 +0600

    comm1

--- frontend/src/components/Profile/Sidebar.jsx
+++ frontend/src/components/Profile/Sidebar.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import { authActions } from '../../Store/auth';
 
 const Sidebar = ({ data }) => {  
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    
-    const handleLogout = () => {
-      localStorage.removeItem('id');
-      localStorage.removeItem('token');
-      localStorage.removeItem('role');
-    
-      dispatch(authActions.logout());
-    
-      navigate('/LogIn');
-    };
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  const role = useSelector((state)=> state.auth.role);
+
   return (
     <div className="bg-[#1e0b37] p-4 rounded flex flex-col items-center justify-between h-auto lg:h-[100%]">
       <div className='flex items-center flex-col justify-center mb-8'>
@@ -39,7 +31,9 @@ const Sidebar = ({ data }) => {
         )}
         <div className='w-full h-1 bg-amber-500 '></div>
       </div>
-      <div className='w-full flex-col items-center justify-center hidden lg:flex'>
+      
+      {role === "user" && (
+        <div className='w-full flex-col items-center justify-center hidden lg:flex'>
         <Link
           to="/profile"
           className='text-xl text-zinc-100 font-semibold w-full py-4 text-center hover:bg-purple-900 rounded transition-all duration-300 mb-4'>
@@ -56,8 +50,34 @@ const Sidebar = ({ data }) => {
           Settings
         </Link>
       </div>
+      )}
+
+      {role === "admin" && <div className='w-full flex-col items-center justify-center hidden lg:flex'>
+        <Link
+          to="/profile"
+          className='text-xl text-zinc-100 font-semibold w-full py-4 text-center hover:bg-purple-900 rounded transition-all duration-300 mb-4'>
+           All Orders
+        </Link>
+        <Link
+          to="/profile/add-game"
+          className='text-xl text-zinc-100 font-semibold w-full py-4 text-center hover:bg-purple-900 rounded transition-all duration-300 mb-4'>
+           Add Game
+        </Link>
+        {/* <Link
+          to="/profile/settings"
+          className='text-xl text-zinc-100 font-semibold w-full py-4 text-center hover:bg-purple-900 rounded transition-all duration-300 mb-4'>
+          Settings
+        </Link> */}
+      </div> }
       <button 
-        onClick={handleLogout}
+        onClick={() =>{
+          dispatch(authActions.logout());
+          dispatch(authActions.changeRole("user"));
+          localStorage.clear("id");
+          localStorage.clear("token");
+          localStorage.clear("role");
+          history("/LogIn");
+        }}
         className='bg-zinc-100 w-full text-zinc-900 font-semibold flex items-center justify-center py-3 rounded hover:bg-amber-500 transition-all duration-300'>
         Log Out <FaArrowRight className="ml-4" />
       </button>
