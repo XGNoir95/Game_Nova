4dca6429b5902deea4eba9b704093fbe7f4ae025
Shuhrid Abrar <139622785+XGNoir95@users.noreply.github.com>
2024-09-13 16:16:19 +0600

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,18 +11,16 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Clear local storage
+
     localStorage.removeItem('id');
     localStorage.removeItem('token');
     localStorage.removeItem('role');
 
-    // Dispatch logout action
     dispatch(authActions.logout());
     dispatch(authActions.changeRole("user"));
-    // Redirect to home page
+
     navigate('/');
 
-    // Close mobile navigation if open
     setMobileNavVisible(false);
   };
 
@@ -33,7 +31,14 @@ const Navbar = () => {
     console.log("Mobile nav visibility:", !mobileNavVisible);
   };
 
-  // Define links based on login status and role
+
+  const handleClick = (action) => {  
+    if (action) {                    
+      action();                      
+    }                                
+    setMobileNavVisible(false);      
+  };   
+
   let links = [
     {
       title: "Home",
@@ -77,11 +82,10 @@ const Navbar = () => {
     <>
       <nav className="z-50 relative flex bg-[#1e0b37] text-white px-8 py-4 items-center justify-between">
         <Link to={"/"} className="flex items-center">
-          <img className="h-8 me-4" src="logo.png" alt="logo" />
+          <img className="h-8 me-4" src="https://s11.gifyu.com/images/S1AKw.png" alt="logo" />
         </Link>
 
         <div className="block md:flex items-center space-x-4">
-          {/* Links aligned to the right */}
           <div className="hidden md:ml-auto md:flex gap-4">
             {links.map((item, i) => (
               <div className="flex items-center" key={i}>
@@ -95,7 +99,7 @@ const Navbar = () => {
                   </Link>
                 ) : item.title === "Log Out" ? (
                   <button
-                    onClick={item.action}
+                    onClick={() => handleClick(item.action)}
                     className="px-2 py-1 bg-pink-500 rounded hover:bg-purple-800 hover:text-white transition-all duration-300"
                   >
                     {item.title}
@@ -139,12 +143,12 @@ const Navbar = () => {
         className={`${
           mobileNavVisible ? "block" : "hidden"
         } h-screen absolute top-20 left-0 w-full flex flex-col items-center justify-center z-50`}
-        style={{ backgroundColor: "#1e0b37" }} // Added inline style here
+        style={{ backgroundColor: "#1e0b37" }} 
       >
         {links.map((item, i) => (
           item.title === "Log Out" ? (
             <button
-              onClick={item.action}
+              onClick={() => handleClick(item.action)}
               className="text-white text-4xl mb-8 font-semibold hover:text-blue-500 transition-all duration-300"
               key={i}
             >
@@ -155,6 +159,7 @@ const Navbar = () => {
               to={item.link}
               className="text-white text-4xl mb-8 font-semibold hover:text-blue-500 transition-all duration-300"
               key={i}
+              onClick={() => handleClick()}
             >
               {item.title}
             </Link>
@@ -166,12 +171,14 @@ const Navbar = () => {
             <Link
               to={"/LogIn"}
               className="px-8 mb-8 text-3xl font-semibold py-2 text-white rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+              onClick={() => handleClick()}
             >
               LogIn
             </Link>
             <Link
               to={"/SignUp"}
               className="px-8 mb-8 text-3xl font-semibold py-2 text-white rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+              onClick={() => handleClick()}
             >
               SignUp
             </Link>
