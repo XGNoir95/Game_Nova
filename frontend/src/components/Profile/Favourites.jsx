e48b81721e5ee830aeeb8c632190293a07d8246c
mahadir04 <142442984+mahadir04@users.noreply.github.com>
2024-09-21 00:50:38 +0600

    MobileNavView

--- frontend/src/components/Profile/Favourites.jsx
+++ frontend/src/components/Profile/Favourites.jsx
@@ -18,15 +18,16 @@ const Favourites = () => {
   return (
     <>      
       {FavouriteGames && FavouriteGames.length === 0 && (
-        <div className='text-4xl font-semibold h-[60vh] text-amber-500 flex items-center justify-center bg-[#1e0b37]'>
+        <div className="text-4xl font-semibold h-[60vh] text-amber-500 flex items-center justify-center bg-[#1e0b37]">
           No Favourite Games
         </div>
       )}
-      <div className="grid grid-cols-4 gap-4">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {FavouriteGames &&
-          FavouriteGames.map((items, i) => (
+          FavouriteGames.map((game, i) => (
             <div key={i}>
-              <GameCard data={items} favourite={true} />
+              <GameCard data={game} favourite={true} />
             </div>
           ))}
       </div>
