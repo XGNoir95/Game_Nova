99eff28cb0de1d1857578d5f53a3e46b6af00e61
mahadir04 <142442984+mahadir04@users.noreply.github.com>
2024-09-21 00:47:31 +0600

    footer changes

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,10 +2,44 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-[#1e0b37] text-white px-8 py-4">
-        <h1 className="text-2x1 front-semibold text-center">
-            &copy;https://youtu.be/dQw4w9WgXcQ?si=nAg488MpbyTB5608</h1>
-    </div>
+    <footer className="bg-[#1e0b37] text-white px-8 py-6">
+      <div className="container mx-auto flex items-center justify-center space-x-8">
+        
+        <img 
+          src="icon.ico" 
+          alt="Game Nova Logo" 
+          style={{ width: "150px", height: "150px" }} 
+        />
+
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-4">
+            &copy; 2024 Game Nova
+          </h1>
+          <p className="text-lg">
+            Made with 💜 by the Game Nova Team
+          </p>
+
+          <div className="flex justify-center space-x-8 mt-4">
+            <a
+              href="https://youtu.be/dQw4w9WgXcQ?si=nAg488MpbyTB5608"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-amber-500 hover:text-pink-500 transition-colors duration-300"
+            >
+              Watch our promo video!
+            </a>
+            <a
+              href="https://github.com/XGNoir95/Game_Nova"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-amber-500 hover:text-pink-500 transition-colors duration-300"
+            >
+              Contact!
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
